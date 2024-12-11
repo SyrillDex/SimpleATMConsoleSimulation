4da6bb692c3145f229d87201cb977a0c0cafaa86
SyrillDex <syrilldexl@gmail.com>
2024-12-11 23:21:19 +0800

    improve the switch statement in main method

--- src/CardInfo.java
+++ src/CardInfo.java
@@ -3,97 +3,97 @@ import java.util.Scanner;
 
 public class CardInfo {
 
-    NumberFormat format = NumberFormat.getCurrencyInstance();
-    Scanner scan = new Scanner(System.in);
+  NumberFormat format = NumberFormat.getCurrencyInstance();
+  Scanner scan = new Scanner(System.in);
 
-    private final int pin = 122820;
-    private final String  accountName = "Syrill Dex Laguerta";
-    double balance = 100000.00;
+  private final int pin = 122820;
+  private final String  accountName = "Syrill Dex Laguerta";
+  double balance = 100000.00;
 
-    public boolean checkPin(String response) {
+  public boolean checkPin(String response) {
 
-        int pinInputLimit = 3;
+    int pinInputLimit = 3;
 
-        while (pinInputLimit > 0){
+    while (pinInputLimit > 0){
 
-            try {
-                System.out.println("Please enter your PIN");
-                response = scan.nextLine();
-                int pinInput = Integer.parseInt(response);
+      try {
+        System.out.println("Please enter your PIN");
+        response = scan.nextLine();
+        int pinInput = Integer.parseInt(response);
 
-                if (pinInput != pin){
-                    pinInputLimit--;
-                    System.out.println("Wrong pin you have " + pinInputLimit + " tries left");
-                    System.out.println();
-                }
-                else {
-                    return true;
-                }
-            }
-            catch (NumberFormatException e) {
-                System.out.println("Please enter a valid pin");
-                pinInputLimit--;
-                System.out.println("Wrong pin you have " + pinInputLimit + " tries left");
-                System.out.println();
-            }
-
+        if (pinInput != pin){
+          pinInputLimit--;
+          System.out.println("Wrong pin you have " + pinInputLimit + " tries left");
+          System.out.println();
         }
-        return false;
-    }
-
-    public void showBalance() {
-        System.out.println("Account name: " + accountName);
+        else {
+          return true;
+        }
+      }
+      catch (NumberFormatException e) {
+        System.out.println("Please enter a valid pin");
+        pinInputLimit--;
+        System.out.println("Wrong pin you have " + pinInputLimit + " tries left");
         System.out.println();
-        System.out.println("Current balance: " + format.format(balance));
+      }
+
+    }
+    return false;
+  }
+
+  public void showBalance() {
+    System.out.println("Account name: " + accountName);
+    System.out.println();
+    System.out.println("Current balance: " + format.format(balance));
+    System.out.println();
+    System.out.println("Press Enter to make another transaction");
+    scan.nextLine();
+  }
+
+  public void depositCash(String response){
+    System.out.println("Please enter the amount you want to deposit");
+    try {
+      response = scan.nextLine();
+      int amount = Integer.parseInt(response);
+
+      if (amount >= 100) {
+        balance += amount;
         System.out.println();
-        System.out.println("Press Enter to make another transaction");
-        scan.nextLine();
+        System.out.println("Your new balance is " + format.format(balance));
+      }
+      else {
+        System.out.println("The minimum deposit amount is 100.00");
+      }
     }
-
-    public void depositCash(String response){
-        System.out.println("Please enter the amount you want to deposit");
-        try {
-            response = scan.nextLine();
-            int amount = Integer.parseInt(response);
-
-            if (amount >= 100) {
-                balance += amount;
-                System.out.println();
-                System.out.println("Your new balance is " + format.format(balance));
-            }
-            else {
-                System.out.println("The minimum deposit amount is 100.00");
-            }
-        }
-        catch (NumberFormatException e) {
-            System.out.println("Please enter a valid amount");
-        }
+    catch (NumberFormatException e) {
+      System.out.println("Please enter a valid amount");
     }
+  }
 
-    public void withdrawCash(String response) {
-        System.out.println("Please enter the amount you want to withdraw");
-        try {
-            response = scan.nextLine();
-            int amount = Integer.parseInt(response);
+  public void withdrawCash(String response) {
+    System.out.println("Please enter the amount you want to withdraw");
+    try {
+      response = scan.nextLine();
+      int amount = Integer.parseInt(response);
 
-            if (amount < 100) {
-                System.out.println("The amount limit is 100.00");
-            }
-            else if (amount % 100 != 0) {
-                System.out.println("The amount must be divisible by 100");
-            }
-            else if (amount > balance) {
-                System.out.println("Not enough balance");
-            }
-            else {
-                balance -= amount;
-                System.out.println();
-                System.out.println("Your new balance is " + format.format(balance));
-                System.out.println("Please get your cash, card, and receipt");
-            }
-        }
-        catch (NumberFormatException e) {
-            System.out.println("Please enter a valid amount");
-        }
+      if (amount < 100) {
+        System.out.println("The amount limit is 100.00");
+      }
+      else if (amount % 100 != 0) {
+        System.out.println("The amount must be divisible by 100");
+      }
+      else if (amount > balance) {
+        System.out.println("Not enough balance");
+      }
+      else {
+        balance -= amount;
+        System.out.println();
+        System.out.println("Your new balance is " + format.format(balance));
+        System.out.println("Please get your cash, card, and receipt");
+      }
     }
+    catch (NumberFormatException e) {
+      System.out.println("Please enter a valid amount");
+    }
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        CardInfo info = new CardInfo();
+    Scanner scan = new Scanner(System.in);
+    CardInfo info = new CardInfo();
 
-        String response;
+    String response;
 
-        System.out.println("Type \"insert\" to insert your card...");
+    System.out.println("Type \"insert\" to insert your card...");
+    response = scan.nextLine().trim().toLowerCase();
+
+    if (response.equals("insert")) {
+      do {
+
+        System.out.println("Please select transaction:");
+        System.out.println("  1. Check balance");
+        System.out.println("  2. Deposit money");
+        System.out.println("  3. Withdraw money");
+        System.out.println();
+        System.out.println("Type \"cancel\" to cancel the transaction...");
         response = scan.nextLine().trim().toLowerCase();
 
-        if (response.equals("insert")) {
-            do {
-
-                System.out.println("Please select transaction:");
-                System.out.println("  1. Check balance");
-                System.out.println("  2. Deposit money");
-                System.out.println("  3. Withdraw money");
-                System.out.println();
-                System.out.println("Type \"cancel\" to cancel the transaction...");
-                response = scan.nextLine().trim().toLowerCase();
-
-                switch (response){
-                    case "1":
-                        if (info.checkPin(response)) {
-                            info.showBalance();
-                        }
-                        else {
-                            System.out.println("Your account is locked");
-                            response = "cancel";
-                        }
-                        break;
-                    case "2":
-                        if (info.checkPin(response)) {
-                            info.depositCash(response);
-                        }
-                        else {
-                            System.out.println("Your account is locked");
-                            response = "cancel";
-                        }
-                        System.out.println();
-                        break;
-                    case "3":
-                        if (info.checkPin(response)) {
-                            info.withdrawCash(response);
-                        }
-                        else {
-                            System.out.println("Your account is locked");
-                            response = "cancel";
-                        }
-                        System.out.println();
-                        break;
-                }
-
-            }while (!response.equals("cancel"));
-            System.out.println("Please get your card...Thank you!");
+        if (response.equals("cancel")) {
+          System.out.println("Transaction canceled. Please retrieve your card.");
+          break;
         }
-        else {
-            System.out.println("Please insert your card");
+        if (info.checkPin(response)) {
+          switch (response) {
+            case "1":
+              info.showBalance();
+              break;
+            case "2":
+              info.depositCash(response);
+              break;
+            case "3":
+              info.withdrawCash(response);
+              break;
+          }
+        }  else {
+          System.out.println("Your account is locked.");
+          response = "cancel";
         }
+
+      }while (!response.equals("cancel"));
     }
+    else {
+      System.out.println("Please insert your card");
+    }
+    scan.close();
+  }
 }
